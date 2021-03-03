091d68abf7a938d1fe8880a39b6a87b9dd9117cf
Jack <jack@theslingsbys.com>
2021-03-03 16:36:02 +0000

    Adds Create initial model

--- Code/EM.py
+++ Code/EM.py
@@ -1,5 +1,39 @@
 import argparse
 import os
+import numpy as np
+
+
+class Model():
+    def __init__(self, pi, m, e):
+        self.pi = pi
+        self.m = m
+        self.e = e
+
+    def __str__(self):
+        outS = "--- pi ---\n"
+        outS += str(self.pi)
+        outS += "\n--- m ---\n"
+        outS += str(self.m)
+        outS += "\n--- e ---\n"
+        outS += str(self.e)
+
+        return outS
+
+def createInitialModel(hidden_states, observeable_states):
+    pi = np.ones(hidden_states)
+    pi /= hidden_states
+
+    m = np.ones((hidden_states, hidden_states))
+    for i in range(hidden_states):
+        m[i] /= hidden_states
+
+    e = np.ones((hidden_states, observeable_states))
+    for i in range(hidden_states):
+        e[i] /= observeable_states
+
+    return Model(pi, m, e)
+
+
 
 def assertFileExists(path):
     if not os.path.isfile(path):
@@ -8,13 +42,22 @@ def assertFileExists(path):
 
 if __name__ =="__main__":
 
-    parser = argparse.ArgumentParser(description='Encoder')
-    parser.add_argument("file_path")
-    parser.add_argument("-t", dest="trial", help="Trial run - won't store output", action='store_true')
-    parser.add_argument("--type", dest="testType", help="Select a test type from: %s"%(",".join(testSuites.keys())),default="live" )
+    parser = argparse.ArgumentParser(description='EM')
+    #parser.add_argument("file_path")
+    parser.add_argument("-k", dest="num_states", help="Number of hidden states")
+    parser.add_argument("-s", dest="num_alpabet", help="Number of symbols in alphabet")
+    #parser.add_argument("-k", dest="num_states", help="Number of hidden states", action='store_true')
+    #parser.add_argument("--type", dest="testType", help="Select a test type from:",default="live" )
     
     args = parser.parse_args()
-    file_path = str(args.file_path)
-    os.environ['TRIAL'] = "T" if args.trial else "F"
+    #os.environ['TRIAL'] = "T" if args.trial else "F"
     
-    # Do something
+    num_hidden_states = int(args.num_states)
+    num_alphabet_sym = int(args.num_alpabet)
+
+    model = createInitialModel(num_hidden_states, num_alphabet_sym)
+
+    print(model)
+    print(np.sum(model.e, axis=1))
+
+   
--- Code/testEM.py
+++ Code/testEM.py
@@ -1,12 +1,25 @@
 import unittest
 
 from EM import *
+import numpy as np
 
 
-class TestEM(unittest.TestCase):
+class TestModelCreate(unittest.TestCase):
+    hidden = 5
+    observe = 3
+    model = createInitialModel(hidden, observe)
     
-    def test_encode_decode(self):
-        self.assertEqual(True, True)
+    def test_pi(self):
+        self.assertEqual(self.model.pi.shape, (self.hidden,))
+        self.assertEqual(np.sum(self.model.pi), 1)
+
+    def test_m(self):
+        self.assertEqual(self.model.m.shape, (self.hidden, self.hidden))
+        self.assertTrue(np.array_equal(np.sum(self.model.m, axis=1), np.ones(self.hidden)))
+
+    def test_e(self):
+        self.assertEqual(self.model.e.shape, (self.hidden, self.observe))
+        self.assertTrue(np.array_equal(np.sum(self.model.e, axis=1), np.ones(self.hidden)))
 
 
 if __name__ == '__main__':
