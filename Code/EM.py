3bf097b0c930df4e3a0b3a74405b91ab83de84aa
Jack <jack@theslingsbys.com>
2021-03-16 16:29:35 +0000

    Finisedish EM draft, which includes new graphs

--- Code/EM.py
+++ Code/EM.py
@@ -182,14 +182,15 @@ def probOfObservations(model, observedSeq):
     return p
 
 if __name__ =="__main__":
-    observedSeq = [0,0,0,1,1,2,3,1,1,1,1,0,3,3,3,3,3,3,3,1,1,1,1,1,1,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,1,1,1,1,1,2,1,2,1,2,1,2,1,2,1,2,0,0,0,1,1,2,3,1,1,1,1,0,3,3,3,3,3,3,3,1,1,1,1,1,1,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,1,1,1,1,1,2,1,2,1,2,1,2,1,2,1,2,0,0,0,1]
-    hidden = 50
+    observedSeq = [0,0,0,1,1,2,3,1,1,1,1,0,3,3,3,3,3,3,3,1,1,1,1,1,1,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,1,1,1,1,1,2,1,2,1,2,1,2,1,2,1,2,0,0,0,1,1,2,1,1,1,1,1,1,1,1,1]
+    #observedSeq = [0,0,0,1,1,2,3,1,1,1,1,0,3,3,3,3,3]
+    hidden = 6
     observeable = 5
 
     for i in range(5):
         _, _, _, probs = estimateModel(observedSeq, hidden, observeable, verbose=True) 
         plt.plot(probs, color='C%d'%i)
-    plt.ylabel('p(path|model)')
+    plt.ylabel('$p(x|\lambda)$')
     plt.yscale('log')
 
     plt.xlabel('Model iteration')
